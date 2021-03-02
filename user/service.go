3b5b5c47ef7dfd29b1aa883023f3324254d11359
ramseshts <ramseshutasoit77@gmail.com>
2021-03-02 21:01:15 +0700

    authentication middleware

--- user/service.go
+++ user/service.go
@@ -11,6 +11,7 @@ type Service interface {
 	Login(input LoginInput) (User, error)
 	IsEmailAvailable(input CheckEmailInput) (bool, error)
 	SaveAvatar(ID int, fileLocation string) (User, error)
+	GetUserByID(ID int) (User, error)
 }
 
 type service struct {
@@ -89,4 +90,15 @@ func (s *service) SaveAvatar(ID int, fileLocation string) (User, error) {
 		return updateUser, err
 	}
 	return updateUser, nil
+}
+
+func (s *service) GetUserByID(ID int) (User, error) {
+	user, err := s.repository.FindByID(ID)
+	if err != nil {
+		return user, nil
+	}
+	if user.ID == 0 {
+		return user, errors.New("No user found on with that ID")
+	}
+	return user, nil
 }
